bd1d3d6dfd026a72d170de147b12681978c4e42f
Samalieruth01 <samalieruth01@gmail.com>
2024-11-02 22:29:33 +0300

    Add files via upload

---
+++ commit7.js
@@ -0,0 +1,6 @@
+//a function that removes duplicate values from an array.
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // [1, 2, 3, 4, 5]
