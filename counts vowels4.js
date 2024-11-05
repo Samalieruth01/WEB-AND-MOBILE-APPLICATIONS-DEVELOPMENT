59b3eceb349b547b88203cda9ac8e5434e33d75a
Samalieruth01 <samalieruth01@gmail.com>
2024-11-05 20:58:34 +0300

    Add files via upload

---
+++ counts vowels4.js
@@ -0,0 +1,14 @@
+// a function that counts the number of vowels in a given string.
+
+function countVowels(str) {
+  const vowels = 'aeiouAEIOU';
+  let count = 0;
+  for (let char of str) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(countVowels("hello world")); // 3
