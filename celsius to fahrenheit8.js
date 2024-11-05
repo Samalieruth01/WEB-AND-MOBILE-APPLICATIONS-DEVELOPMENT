e888d1d4e995b899e41c9a030107b1a920bb9fd9
Samalieruth01 <samalieruth01@gmail.com>
2024-11-05 21:02:52 +0300

    Add files via upload

---
+++ celsius to fahrenheit8.js
@@ -0,0 +1,7 @@
+// a function to convert temperatures from Celsius to Fahrenheit.
+
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9/5) + 32;
+}
+
+console.log(celsiusToFahrenheit(0)); // 32
