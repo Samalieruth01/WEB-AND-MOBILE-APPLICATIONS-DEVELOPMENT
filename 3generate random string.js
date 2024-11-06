479b502762b60cc5b7c98b24d39480d087252aa2
Samalieruth01 <samalieruth01@gmail.com>
2024-11-06 09:03:47 +0300

    Add files via upload

---
+++ 3generate random string.js
@@ -0,0 +1,12 @@
+// a function that generates a random string of a given length.
+
+function generateRandomString(length) {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
+
+console.log(generateRandomString(10)); // e.g., "aBcDeFgHiJ"
